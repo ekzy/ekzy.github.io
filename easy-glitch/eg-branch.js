b275546ba9c7353e6989a6bca0f567f8ea58a323
Eva Perman <exipolar@gmail.com>
2015-06-30 23:19:38 -0400

    added comment

--- easy-glitch/eg-branch.js
+++ easy-glitch/eg-branch.js
@@ -1,129 +1,130 @@
 function EasyGlitch(canvasNode){
- var s = this; //s is for self
- s.canvas = canvasNode;
- s.buffer = document.createElement("canvas");
- s.context = s.canvas.getContext("2d");
- s.buffcxt = s.buffer.getContext("2d");
- s.img = new Image();
- s.sourceImg = s.img;
- s.canDraw = false;
- s.drawOnLoad = true;
- var undoData;
- s.loadImage = function(){
- 	s.canDraw = true;
- 	if(s.drawOnLoad){
- 		undoData = undefined;
- 		s.canvas.height = s.buffer.height = s.img.height;
- 		s.canvas.width = s.buffer.width = s.img.width;
- 		s.context.drawImage(s.img,0,0);
- 	}
- }
- s.reset = function (){
- 	s.loadImage();
- }
- s.img.onload = s.loadImage;
- s.img.src = "/hk.jpg";
- s.sq = [[25,4],[50,2],[100,1]];
- s.cSq = 0;
- s.sx = s.sy = s.dx = s.dy = 0;
- var randomInt = function(integer,mod){
- 	mod = (typeof mod === "undefined") ? 1 : mod;
- 	return Math.floor( integer * Math.random()/mod ) * mod;
- }
- var randomFloat = function(floaty){
- 	return floaty * Math.random();
- }
- s.getcSq = function(sq){
- 	var denom=0;
-	for(var i in sq){
-		denom+=sq[i][1];
-	}
-	var value=randomFloat(denom);
-	var i = 0;
-	do {
-		if(value > sq[i][1]){
-			value -= sq[i][1];
-			i++;
-		}else{
-			return sq[i][0];
-		}
-	} while (value > 0);
- }
- s.preserve = false;
- s.manualSize = false;
- s.sloppy = false;
- s.cSq = 25;
- s.drawScramble = function(e){
- 	s.sourceImg = (s.preserve)?s.canvas:s.img;
- 	if((e.which===1 && e.buttons === undefined)|| e.buttons % 2 == 1){
- 		s.cSq = (s.manualSize) ? s.cSq - (!s.sloppy) * (s.cSq % 1) : s.getcSq(s.sq);
- 		s.sx=randomInt(s.sourceImg.width,s.cSq);
-		s.sy=randomInt(s.sourceImg.height,s.cSq);
-		s.dx=Math.floor(e.layerX/s.cSq) * s.cSq;
-		s.dy=Math.floor(e.layerY/s.cSq) * s.cSq;
-
-		if(s.preserve){
-			s.buffcxt.drawImage(s.sourceImg, s.sx ,s.sy ,s.cSq, s.cSq, s.sx , s.sy, s.cSq, s.cSq);
-			s.context.drawImage(s.sourceImg, s.dx ,s.dy ,s.cSq, s.cSq, s.sx , s.sy, s.cSq, s.cSq);
-			s.context.drawImage(s.buffer, s.sx ,s.sy ,s.cSq, s.cSq, s.dx , s.dy, s.cSq, s.cSq);
-		} else {
-			s.context.drawImage(s.sourceImg, s.sx ,s.sy ,s.cSq, s.cSq, s.dx , s.dy, s.cSq, s.cSq);
-		}
- 	}
- }
-	s.playing = false;
-	s.interval = null;
-	s.randomScramble = function(){
-		var e;
-		s.playing=!s.playing;
-		if(s.playing){
-			s.setUndo();
-			s.interval = window.setInterval( function(){
-				e={which:1,layerX:randomInt(s.img.width),
-				layerY:randomInt(s.img.height)
-				};
-				s.drawScramble(e);
-			},1000/200);
-		} else {
-			clearInterval(s.interval);
-		}
- 	}
- 	s.canvas.onmousemove = s.canvas.onmousdown = s.drawScramble;
- 	s.fileNode = document.createElement("input");
- 	s.fileNode.setAttribute("type","file");
- 	s.openFile = function(){
- 		s.fileNode.value = "";
- 		s.fileNode.click();
- 	}
- 	s.fr = new FileReader();
- 	s.fr.onload = function(){
- 		s.img.src = s.fr.result;
- 	}
- 	s.fileNode.onchange = function(){
- 		if( this.value != ""){
- 			s.fr.readAsDataURL( s.fileNode.files[0] );
- 		}
- 	}
- 	s.image = "";
- 	s.saveImage = function(){
- 		s.image = s.canvas.toDataURL("image/jpeg");
- 		window.open(s.image); //don't crash please!!!
- 	}
- 	s.changeTileSize = function(){
- 		//don't know how to do yet with current implementation but OO helps!!!
- 	}
- 	
- 	s.setUndo = function() {
- 		undoData = s.context.getImageData(0,0,canvas.width,canvas.height);
- 	}
- 	canvas.onmousedown = s.setUndo;
- 	s.undo = function(){
- 		s.context.putImageData(undoData,0,0);
- 	}
- 	s.makeObama = function(){
- 		s.img.src="/obama.jpg";
- 	}
- 	s.makeHK = function(){
- 		s.img.src="/hk.jpg";
- 	}
+    var s = this; //s is for self
+    s.canvas = canvasNode;
+    s.buffer = document.createElement("canvas");
+    s.context = s.canvas.getContext("2d");
+    s.buffcxt = s.buffer.getContext("2d");
+    s.img = new Image();
+    s.sourceImg = s.img;
+    s.canDraw = false;
+    s.drawOnLoad = true;
+    var undoData;
+    s.loadImage = function(){
+        s.canDraw = true;
+        if(s.drawOnLoad){
+            undoData = undefined;
+            s.canvas.height = s.buffer.height = s.img.height;
+            s.canvas.width = s.buffer.width = s.img.width;
+            s.context.drawImage(s.img,0,0);
+        }
+    }
+    s.reset = function (){
+        s.loadImage();
+    }
+    s.img.onload = s.loadImage;
+    s.img.src = "/hk.jpg";
+    //TODO ADD string support and add dimension support
+    //Should also add advanced mode.
+    s.sq = [[25,4],[50,2],[100,1]];
+    s.cSq = 0;
+    s.sx = s.sy = s.dx = s.dy = 0;
+    var randomInt = function(integer,mod){
+        mod = (typeof mod === "undefined") ? 1 : mod;
+        return Math.floor( integer * Math.random()/mod ) * mod;
+    }
+    var randomFloat = function(floaty){
+        return floaty * Math.random();
+    }
+    s.getcSq = function(sq){
+        var denom=0;
+    for(var i in sq){
+        denom+=sq[i][1];
+    }
+    var value=randomFloat(denom);
+    var i = 0;
+    do {
+        if(value > sq[i][1]){
+            value -= sq[i][1];
+            i++;
+        }else{
+            return sq[i][0];
+        }
+    } while (value > 0);
+    }
+    s.preserve = false;
+    s.manualSize = false;
+    s.sloppy = false;
+    s.cSq = 25;
+    s.drawScramble = function(e){
+        s.sourceImg = (s.preserve)?s.canvas:s.img;
+        if((e.which===1 && e.buttons === undefined)|| e.buttons % 2 == 1){
+            s.cSq = (s.manualSize) ? s.cSq - (!s.sloppy) * (s.cSq % 1) : s.getcSq(s.sq);
+            s.sx=randomInt(s.sourceImg.width,s.cSq);
+            s.sy=randomInt(s.sourceImg.height,s.cSq);
+            s.dx=Math.floor(e.layerX/s.cSq) * s.cSq;
+            s.dy=Math.floor(e.layerY/s.cSq) * s.cSq;
+            if(s.preserve){
+                s.buffcxt.drawImage(s.sourceImg, s.sx ,s.sy ,s.cSq, s.cSq, s.sx , s.sy, s.cSq, s.cSq);
+                s.context.drawImage(s.sourceImg, s.dx ,s.dy ,s.cSq, s.cSq, s.sx , s.sy, s.cSq, s.cSq);
+                s.context.drawImage(s.buffer, s.sx ,s.sy ,s.cSq, s.cSq, s.dx , s.dy, s.cSq, s.cSq);
+            } else {
+                s.context.drawImage(s.sourceImg, s.sx ,s.sy ,s.cSq, s.cSq, s.dx , s.dy, s.cSq, s.cSq);
+            }
+        }
+    }
+    s.playing = false;
+    s.interval = null;
+    s.randomScramble = function(){
+        var e;
+        s.playing=!s.playing;
+        if(s.playing){
+            s.setUndo();
+            s.interval = window.setInterval( function(){
+                e={which:1,layerX:randomInt(s.img.width),
+                    layerY:randomInt(s.img.height)
+                };
+                s.drawScramble(e);
+            },1000/200);
+        } else {
+            clearInterval(s.interval);
+        }
+        }
+        s.canvas.onmousemove = s.canvas.onmousdown = s.drawScramble;
+        s.fileNode = document.createElement("input");
+        s.fileNode.setAttribute("type","file");
+        s.openFile = function(){
+            s.fileNode.value = "";
+            s.fileNode.click();
+        }
+        s.fr = new FileReader();
+        s.fr.onload = function(){
+            s.img.src = s.fr.result;
+        }
+        s.fileNode.onchange = function(){
+            if( this.value != ""){
+                s.fr.readAsDataURL( s.fileNode.files[0] );
+            }
+        }
+        s.image = "";
+        s.saveImage = function(){
+            s.image = s.canvas.toDataURL("image/jpeg");
+            window.open(s.image); //don't crash please!!!
+        }
+        s.changeTileSize = function(){
+            //don't know how to do yet with current implementation but OO helps!!!
+        }
+        
+        s.setUndo = function() {
+            undoData = s.context.getImageData(0,0,canvas.width,canvas.height);
+        }
+        canvas.onmousedown = s.setUndo;
+        s.undo = function(){
+            s.context.putImageData(undoData,0,0);
+        }
+        s.makeObama = function(){
+            s.img.src="/obama.jpg";
+        }
+        s.makeHK = function(){
+            s.img.src="/hk.jpg";
+        }
 }
